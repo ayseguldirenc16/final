2dfe99d52ed5a5f512418ef355907a8645c2e952
Ayşegül Direnç <100558352+ayseguldirenc16@users.noreply.github.com>
2023-09-14 10:59:22 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Nav from './companents/Nav'
+import Header from './companents/Header'
+import Footer from './companents/Footer'
+import './nav.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import IconMagnifyingGlass from './companents/IconMagnifyingGlass'
+import IconBxUserCircle from './companents/IconBxUserCircle'
+import IconBxShoppingBag from './companents/IconBxShoppingBag'
+import Article from './companents/Article'
+import Section from './companents/Section'
+
+
+
+
+
+
+
+
+
+
+function App() {
+
+  return (
+    <> 
+    <Nav /> 
+
+    <Header /> 
+    <Article /> 
+    <Section />
+    <Footer />
+    
+    </>
+  )
+}
+
+export default App
